a3c221775558df33d9bb0bcc2de2b51a68b8c80d
Ikims <107883257+Yanis-Gerst@users.noreply.github.com>
2022-08-01 10:08:30 +0200

    Fixed Link

--- dist/index.578fea3e.js
+++
@@ -1,6 +0,0 @@
-let gameContainer = document.querySelector(".game-container");
-gameContainer = new Overlord(gameContainer);
-gameContainer.init() //Test
-;
-
-//# sourceMappingURL=index.578fea3e.js.map
