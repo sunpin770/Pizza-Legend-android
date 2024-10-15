8e97252caa6ed65786cc60498c6c64103b80157f
sunpin770 <246813579rpc@gmail.com>
2024-10-15 17:16:27 -0600

    Update input.js
    Added touch input

--- Script/input.js
+++ Script/input.js
@@ -1,38 +1,73 @@
+class DirectionInput {
+  constructor() {
+    this.heldDirection = [];
 
-class Input {
-    constructor(config) {
-        this.callStackDirection = []
-        this.keyDirection = {
-            "z": "up",
-            "s": "down",
-            "q": "left",
-            "d": "right"
-        }
-        
-    }
-    
-    get direction() {
-        return this.callStackDirection[0]
-    }
+    this.map = {
+      ArrowUp: "up",
+      ArrowDown: "down",
+      ArrowLeft: "left",
+      ArrowRight: "right",
 
-    init() {
-        document.addEventListener("keypress", e => {
-            let direction = this.keyDirection[e.key]
+      KeyW: "up",
+      KeyS: "down",
+      KeyA: "left",
+      KeyD: "right",
+    };
 
-            if (direction && this.callStackDirection.indexOf(direction) == -1) {
-                this.callStackDirection.unshift(direction)
-            }
-            
-        })
+    this.keyUpEvent = (e) => {
+      const dir = this.map[e.code];
+      const index = this.heldDirection.indexOf(dir);
+      if (index > -1) {
+        this.heldDirection.splice(index, 1);
+      }
+    };
 
-        document.addEventListener("keyup", e => {
-            const direction = this.keyDirection[e.key]
-            const index = this.callStackDirection.indexOf(direction)
+    this.keyDownEvent = (e) => {
+      const dir = this.map[e.code];
+      if (dir && this.heldDirection.indexOf(dir) === -1) {
+        this.heldDirection.unshift(dir);
+      }
+    };
 
-            if (index > -1) {
-                this.callStackDirection.splice(index, 1)
-            }
-           
-        })
-    }
-} 
+    // Touch event handlers
+    this.touchStartEvent = (dir) => {
+      if (this.heldDirection.indexOf(dir) === -1) {
+        this.heldDirection.unshift(dir);
+      }
+    };
+
+    this.touchEndEvent = (dir) => {
+      const index = this.heldDirection.indexOf(dir);
+      if (index > -1) {
+        this.heldDirection.splice(index, 1);
+      }
+    };
+  }
+
+  get direction() {
+    return this.heldDirection[0];
+  }
+
+  reMap() {
+    this.map = playerState.directions;
+    document.removeEventListener("keyup", this.keyUpEvent);
+    document.removeEventListener("keydown", this.keyDownEvent);
+    this.init();
+  }
+
+  init() {
+    document.addEventListener("keydown", this.keyDownEvent);
+    document.addEventListener("keyup", this.keyUpEvent);
+
+    // Add touch event listeners for buttons
+    document.getElementById("up").addEventListener("touchstart", () => this.touchStartEvent("up"));
+    document.getElementById("down").addEventListener("touchstart", () => this.touchStartEvent("down"));
+    document.getElementById("left").addEventListener("touchstart", () => this.touchStartEvent("left"));
+    document.getElementById("right").addEventListener("touchstart", () => this.touchStartEvent("right"));
+
+    document.getElementById("up").addEventListener("touchend", () => this.touchEndEvent("up"));
+    document.getElementById("down").addEventListener("touchend", () => this.touchEndEvent("down"));
+    document.getElementById("left").addEventListener("touchend", () => this.touchEndEvent("left"));
+    document.getElementById("right").addEventListener("touchend", () => this.touchEndEvent("right"));
+  }
+}
